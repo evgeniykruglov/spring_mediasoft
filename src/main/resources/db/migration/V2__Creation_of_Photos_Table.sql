bcdbb55a7104d9383198d004e19c124b7df60363
e.kruglov <zhe.73@bk.ru>
2021-10-10 20:21:00 +0400

    L5: Added migrations V2__Creation_of_Photos_Table.sql V3__Creation_of_UsersPhotos_Table.sql V4__Adding_contraint_to_Users_And_Photos_tables.sql

---
+++ src/main/resources/db/migration/V2__Creation_of_Photos_Table.sql
@@ -0,0 +1,6 @@
+create table IF NOT EXISTS homework.photos
+(
+    id   bigint PRIMARY KEY not null,
+    path varchar(16)        not null
+);
+
---
+++ src/main/resources/db/migration/V3__Creation_of_UsersPhotos_Table.sql
@@ -0,0 +1,5 @@
+create table IF NOT EXISTS homework.users_photos
+(
+    user_id  bigint not null,
+    photo_id bigint not null
+);
