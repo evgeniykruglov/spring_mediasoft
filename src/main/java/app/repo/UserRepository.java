a579c84b08025582e8be52c3aaabbf8e0fffa1ba
e.kruglov <zhe.73@bk.ru>
2021-10-19 22:07:59 +0400

    L8: Added getUsersByEmail and getUsersByName

--- src/main/java/app/repo/UserRepository.java
+++ src/main/java/app/repo/UserRepository.java
@@ -2,9 +2,18 @@ package app.repo;
 
 import app.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where u.name like %?1%")
+    List<User> findUsersByUserName(String name);
+
+    @Query("select u from User u where u.email like %?1%")
+    List<User> findUsersByUserEmail(String email);
 
 }
